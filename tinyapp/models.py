86f5e50d4e5ea4e08d1c344a1c167953026a26d3
Giovani Candido <giovcandido@outlook.com>
2022-12-06 12:45:53 -0300

    Change line break in tinyapp models.py

--- tinyapp/models.py
+++ tinyapp/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class TinyMessage(models.Model):
     content = models.TextField(
-        max_length=300, help_text='Enter a tiny message')
+        max_length=300,
+        help_text='Enter a tiny message'
+    )
 
     class Meta:
         ordering = ['-id']
